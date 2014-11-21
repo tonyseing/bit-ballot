0d9797da6ca3944739eca8401bea13aed2a102d7
tony seing <tony.seing@gmail.com>
2014-11-21 10:07:28 -0500

    adds function to transition eleciton from one state to the next

--- database.rb
+++ database.rb
@@ -2,6 +2,9 @@ require 'mongoid'
 
 # set database config and connect to database here
 
+Mongoid.load!("./mongoid.yml", :development)
+
+
 class Organizer
   include Mongoid::Document
   field :name, type: String
@@ -14,15 +17,35 @@ class Election
   include Mongoid::Document
   field :name, type: String
   field :description, type: String
-  field :state, type: String, default: "pre-registration"
+  field :state, type: String, default: "registration"
 
   validates_inclusion_of :state, in: ["registration", "commenced", "concluded"]
+
+
   embeds_many :voters
   embedded_in :organizer
+
+  def move_next_state!
+    if self.state == "registration"
+      self.state = "commenced"
+      self.save!
+    elsif self.state == "commenced"
+      self.state = "concluded"
+      self.save!
+    else
+      raise "Election already ended"
+    end
+  end
+  
+  def is_over?
+    self.state == "concluded"
+  end
+
 end
 
 class Voter
   include Mongoid::Document
   field :encrypted_address, type: String
   field :received_token, type: Boolean, default: false
+  embedded_in :election
 end
